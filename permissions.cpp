37efd511c84904ce95cf533a94a88e93bbbad3e2
sharvey <bundito@users.noreply.github.com>
2018-09-21 19:08:24 -0500

    Added more checks; program now takes input as test
    Usage: permissions <user>:<group>:<value>
    
    Will test and report all three values.

--- permissions.cpp
+++ permissions.cpp
@@ -1,20 +1,23 @@
 /*
  *  Copyright (C) 2018 Scott Harvey <scott@spharvey.me>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do
+ *  so, subject to the following conditions:
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ *  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
  */
 
 #include <QString>
@@ -28,7 +31,6 @@ Permissions::Permissions(QString p) :
     m_group(QString()),
     m_value(0)
 {
-    qDebug() << p;
     parsePermissions(p);
 }
 
@@ -41,16 +43,23 @@ void Permissions::parsePermissions(QString const &p) {
         return;
     }
 
-    bool ok;
-    int octal = segments[2].toInt(&ok, 8);
-    if (!ok) {
+    if (segments[0].isEmpty() || segments[1].isEmpty()) {
         m_valid = false;
-    } else {
-        m_value = octal;
+        return;
     }
 
-    // We have exactly three segments and the third one is valid octal,
+    bool ok;
+    int octal = segments[2].toInt(&ok, 8);
+    if (!ok || octal == 0) {
+        m_valid = false;
+        return;
+    } else {
+        m_value = segments[2].toInt(&ok, 10);
+    }
+
+    // We have exactly three segments and the third is valid octal,
     // so we can declare the string valid and set the user and group names
+    m_valid = true;
     m_username = segments[0];
     m_group = segments[1];
 
@@ -59,35 +68,20 @@ void Permissions::parsePermissions(QString const &p) {
 }
 
 bool Permissions::isValid() {
-        return m_valid;
-        }
+    return m_valid;
+}
 
 QString Permissions::username() {
-    if (m_valid) {
-        return m_username;
-    } else {
-        invalidAccess();
-    }
+    return m_username;
 }
 
 QString Permissions::group() {
-    if (m_valid) {
-        return m_group;
-    } else {
-        invalidAccess();
-    }
+    return m_group;
 }
 
 int Permissions::value() {
-    if (m_valid) {
-        return m_value;
-    } else {
-        invalidAccess();
-    }
+    return m_value;
 }
 
-void Permissions::invalidAccess() {
-    printf("Error: Not a valid permission string");
-    exit(1);
-}
+
 
--- permissions.h
+++ permissions.h
@@ -1,35 +1,37 @@
 /*
  *  Copyright (C) 2018 Scott Harvey <scott@spharvey.me>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do
+ *  so, subject to the following conditions:
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ *  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
  */
 
 #ifndef PERMISSIONS_H
 #define PERMISSIONS_H
 
-#include <QObject>
+#include <QString>
 
 /**
  * @brief The Permissions class takes a QString @p in the form of <user>:<group>:<permissions>, checks
  * it for validity, and makes the three components available indivdually.
  */
 
-class Permissions : public QObject
+class Permissions
 {
-    Q_OBJECT
 
 public:
 
@@ -37,9 +39,7 @@ public:
  * Splits the string @p at the colon (":") into separate elements for <user>, <group>, and <value> (permissions), where
  * <value> is returned as an **octal** number.
  */
-    Permissions();
     Permissions(QString p);
-
 
     bool m_valid;
     QString m_username;
@@ -56,10 +56,6 @@ public:
  * is not valid
 */
     void invalidAccess();
-
-
-
-
 
 };
 
